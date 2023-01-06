ee47d10895d5cbca60828929693c0e21e655efd0
IliaNoah <vaguaman86@gmail.com>
2023-01-06 17:23:07 +0330

    refactor

--- script.js
+++ script.js
@@ -9,19 +9,19 @@ toggle.addEventListener('click', function() {
     if(moon.classList.contains('hidden')) {
         document.querySelector('body').style.backgroundColor = '#262626';
         document.querySelector('.card').style.backgroundColor = '#464646';
-        document.querySelector('.card h1').style.color = '#fdffcd';
+        document.querySelector('.title').style.color = '#E6E5A3';
         document.querySelector('.code').style.color = '#fdffcd';
         document.querySelector('.valid').style.color = '#fdffcd';
         document.querySelector('.date').style.color = '#fdffcd';
         document.querySelector('.id').style.color = '#fdffcd';
 
     } else {
-        document.querySelector('body').style.backgroundColor = '#9fd3c7';
+        document.querySelector('body').style.backgroundColor = '#EAD2AC';
         document.querySelector('.card').style.backgroundColor = '#e7eaf6';
-        document.querySelector('.title').style.color = '#233142';
-        document.querySelector('.code').style.color = '#455d7a';
-        document.querySelector('.valid').style.color = '#385170';
-        document.querySelector('.date').style.color = '#113f67';
-        document.querySelector('.id').style.color = '#113f67';
+        document.querySelector('.title').style.color = '#000000';
+        document.querySelector('.code').style.color = '#282A3A';
+        document.querySelector('.valid').style.color = '#735F32';
+        document.querySelector('.date').style.color = '#C69749';
+        document.querySelector('.id').style.color = '#C69749';
     }
 });
